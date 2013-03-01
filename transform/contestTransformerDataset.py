b33ce6d26985e44f01843d460fa2d311b3d5c973
Xavier Bouthillier/ <xavier.bouthillier@gmail.com>
2013-02-28 23:12:41 -0500

    added transform module based on P-L Carrier's work

---
+++ transform/contestTransformerDataset.py
@@ -0,0 +1,109 @@
+__authors__ = "Ian Goodfellow"
+__copyright__ = "Copyright 2010-2012, Universite de Montreal"
+__credits__ = ["Ian Goodfellow"]
+__license__ = "3-clause BSD"
+__maintainer__ = "Ian Goodfellow"
+__email__ = "goodfeli@iro"
+from pylearn2.datasets.dataset import Dataset
+
+# Modified by plcarrier so that when get_test_set is called, 
+# the raw dataset is returned instead of the TransformerDataset so
+# that no transformation is performed on the test data
+
+class TransformerDataset(Dataset):
+    """
+        A dataset that applies a transformation on the fly
+        as examples are requested.
+    """
+    def __init__(self, raw, transformer, cpu_only = False,
+            space_preserving=False):
+        """
+            raw: a pylearn2 Dataset that provides raw data
+            transformer: a pylearn2 Block to transform the data
+        """
+        self.__dict__.update(locals())
+        del self.self
+
+    def get_batch_design(self, batch_size, include_labels=False):
+        raw = self.raw.get_batch_design(batch_size, include_labels)
+        if include_labels:
+            X, y = raw
+        else:
+            X = raw
+        X = self.transformer.perform(X)
+        if include_labels:
+            return X, y
+        return X
+
+    def get_test_set(self):
+        return self.raw.get_test_set()
+
+
+    def get_batch_topo(self, batch_size):
+        """
+        If the transformer has changed the space, we don't have a good
+        idea of how to do topology in the new space.
+        If the transformer just changes the values in the original space,
+        we can have the raw dataset provide the topology.
+        """
+        X = self.get_batch_design(batch_size)
+        if self.space_preserving:
+            return self.raw.get_topological_view(X)
+        return X.reshape(X.shape[0],X.shape[1],1,1)
+
+    def iterator(self, mode=None, batch_size=None, num_batches=None,
+                 topo=None, targets=None, rng=None):
+
+        raw_iterator = self.raw.iterator(mode, batch_size, num_batches, topo, targets, rng)
+
+        final_iterator = TransformerIterator(raw_iterator, self)
+
+        return final_iterator
+
+    def has_targets(self):
+        return self.raw.y is not None
+
+    def adjust_for_viewer(self, X):
+        if self.space_preserving:
+            return self.raw.adjust_for_viewer(X)
+        return X
+
+    def get_weights_view(self, *args, **kwargs):
+        if self.space_preserving:
+            return self.raw.get_weights_view(*args, **kwargs)
+        raise NotImplementedError()
+
+    def get_topological_view(self, *args, **kwargs):
+        if self.space_preserving:
+            return self.raw.get_weights_view(*args, **kwargs)
+        raise NotImplementedError()
+
+    def adjust_to_be_viewed_with(self, *args, **kwargs):
+        return self.raw.adjust_to_be_viewed_with(*args, **kwargs)
+
+
+class TransformerIterator(object):
+
+    def __init__(self, raw_iterator, transformer_dataset):
+        self.raw_iterator = raw_iterator
+        self.transformer_dataset = transformer_dataset
+        self.stochastic = raw_iterator.stochastic
+        self.uneven = raw_iterator.uneven
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+
+        raw_batch = self.raw_iterator.next()
+
+        if self.raw_iterator._targets:
+            rval = (self.transformer_dataset.transformer.perform(raw_batch[0]), raw_batch[1])
+        else:
+            rval = self.transformer_dataset.transformer.perform(raw_batch)
+
+        return rval
+
+    @property
+    def num_examples(self):
+        return self.raw_iterator.num_examples
---
+++ transform/transformer.py
@@ -0,0 +1,233 @@
+import numpy as np
+from scipy import ndimage, misc
+
+class TransformationPipeline:
+    """
+        Apply transformations sequentially
+    """
+    # shape should we switched to Conv2DSpace or something similar to make space conversion more general
+    def __init__(self,transformations,seed=None,shape=None):
+        self.transformations = transformations
+        self.shape = shape
+
+        if seed:
+            np.random.RandomState(seed)
+
+    def perform(self, X):
+        shape = X.shape
+        if self.shape:
+            X = X.reshape(tuple([X.shape[0]]+self.shape))
+
+        for transformation in self.transformations:
+            X = transformation.perform(X)
+
+        return X.reshape(shape)
+
+class TransformationPool:
+    """
+        Apply one of the transformations given the probability distribution
+        default : equal probability for every transformations
+    """
+    def __init__(self,transformations,p_distribution=None,seed=None):
+        self.transformations = transformations
+
+        if p_distribution:
+            self.p_distribution = p_distribution
+        else:
+            self.p_distribution = np.ones(len(transformations))/(.0+len(transformations))
+
+        if seed:
+            np.random.RandomState(seed)
+
+    def perform(self, X):
+        # randomly pick one transformation according to probability distribution
+        i = np.array(self.p_distribution).cumsum().searchsorted(np.random.sample(1))
+
+        return self.transformations[i](X)
+
+def gen_mm_random(f,args,min,max,max_iteration=50):
+    if not min:
+        min = -1e99
+
+    if not max:
+        max = 1e99
+
+    i = 0
+    r = f(**args)
+    while (r < min or max < r) and i < max_iteration:
+        r = f(**args)
+        i += 1
+
+    if i >= max_iteration:
+        raise RuntimeError("Were not able to generate a random value between "+str(min)+" and "+str(max)+" in less than "+str(i)+" iterations")
+
+    return r
+
+class RandomTransformation:
+    def __init__(self,p):
+        self.p = p
+
+    def perform(self,X):
+        
+        for i in xrange(X.shape[0]):
+            p = np.random.random(1)[0]
+            if p < self.p:
+                X[i] = self.transform(X[i])
+
+        return X
+
+    def transform(self,X):
+        return X
+
+class Translation(RandomTransformation):
+    def __init__(self,p=1,mean=0.0,std=1.0,min=None,max=None):
+        RandomTransformation.__init__(self,p)
+        self.__dict__.update(locals())
+
+        self.I = np.identity(2)
+
+    def transform(self,x):
+
+        rx = gen_mm_random(np.random.normal,dict(loc=self.mean,scale=self.std),self.min,self.max)
+        ry = gen_mm_random(np.random.normal,dict(loc=self.mean,scale=self.std),self.min,self.max)
+
+        return ndimage.affine_transform(x,self.I,offset=(rx,ry))
+
+class Scaling(RandomTransformation):
+    def __init__(self,p=1,mean=1.0,std=0.1,min=0.1,max=None):
+        RandomTransformation.__init__(self,p)
+        self.__dict__.update(locals())
+
+        pass
+
+    def transform(self, x):
+
+        zoom = gen_mm_random(np.random.normal,dict(loc=self.mean,scale=self.std),self.min,self.max)
+        shape = x.shape
+
+        im = ndimage.zoom(x,zoom)
+        try:
+            im = self._crop(im,shape)
+        except ValueError as e:
+            im = x
+
+        if im.shape[0] != shape[0]:
+            return x
+
+        return im
+
+    def _crop(self,x,shape):
+        # noise or black pixels?
+#        y = np.random.rand(*shape)*255
+        y = np.zeros(shape)
+
+        if x.shape[0] < shape[0]:
+            y[int((y.shape[0]-x.shape[0])/2.0+0.5):int(-(y.shape[0]-x.shape[0])/2.0),
+              int((y.shape[1]-x.shape[1])/2.0+0.5):int(-(y.shape[1]-x.shape[1])/2.0)] = x
+        else:
+            y = x[int((x.shape[0]-y.shape[0])/2.0+0.5):int(-(x.shape[0]-y.shape[0])/2.0),
+                  int((x.shape[1]-y.shape[1])/2.0+0.5):int(-(x.shape[1]-y.shape[1])/2.0)]
+        return y
+
+class Rotation(RandomTransformation):
+    def __init__(self,p=1,mean=0.0,std=10.0,min=None,max=None):
+        RandomTransformation.__init__(self,p)
+        self.__dict__.update(locals())
+        pass
+
+    def transform(self,x):
+
+        degree = gen_mm_random(np.random.normal,dict(loc=self.mean,scale=self.std),self.min,self.max)
+
+        return ndimage.rotate(x,degree,reshape=False)
+
+class Gaussian_Noise(RandomTransformation):
+    def __init__(self,p,sigma=3):
+        RandomTransformation.__init__(self,p)
+        self.__dict__.update(locals())
+        pass
+
+    def transform(self,x):
+
+        return ndimage.gaussian_filter(x,self.sigma)
+
+class Sharpening(RandomTransformation):
+    def __init__(self,p,sigma1=3,sigma2=1,alpha=30):
+        RandomTransformation.__init__(self,p)
+        self.__dict__.update(locals())
+        pass
+
+    def transform(self,x):
+
+        blurred_l = ndimage.gaussian_filter(x, self.sigma1)
+
+        filter_blurred_l = ndimage.gaussian_filter(blurred_l, self.sigma2)
+        return blurred_l + self.alpha * (blurred_l - filter_blurred_l)
+
+class Denoising(RandomTransformation):
+    def __init__(self,p,sigma1=2,sigma2=3,alpha=0.4):
+        RandomTransformation.__init__(self,p)
+        self.__dict__.update(locals())
+        pass
+
+    def transform(self,x):
+        noisy = x + self.alpha * x.std() * np.random.random(x.shape)
+
+        gauss_denoised = ndimage.gaussian_filter(noisy, self.sigma1)
+
+        return ndimage.median_filter(noisy, self.sigma2)
+
+class Flipping(RandomTransformation):
+    def __init__(self,p=1,min=None,max=None):
+        RandomTransformation.__init__(self,p)
+        self.__dict__.update(locals())
+        pass
+
+    def transform(self,x):
+
+        return np.fliplr(x)
+      
+class Occlusion:
+    def __init__(self,p=0.10,nb=10,mean=25,std=10.0,min=2,max=None):
+        self.__dict__.update(locals())
+        pass
+
+    def perform(self,X):
+        
+        for i in xrange(X.shape[0]):
+            X[i] = self.transform(X[i])
+
+        return X
+
+    def transform(self,image):
+        nb = np.sum(np.random.random(self.nb)<self.p)
+        for i in xrange(nb):
+            x = gen_mm_random(np.random.rand,dict(d0=1),
+                              0.0,(image.shape[0]-self.min-1)/(.0+image.shape[0]))
+            y = gen_mm_random(np.random.rand,dict(d0=1),
+                              0.0,(image.shape[1]-self.min-1)/(.0+image.shape[1]))
+            dx = gen_mm_random(np.random.rand,
+                               dict(loc=self.mean,scale=self.std),
+                               self.min,self.max)
+            dy = gen_mm_random(np.random.rand,
+                               dict(loc=self.mean,scale=self.std),
+                               self.min,self.max)
+
+            image[x:x+dx,y:y+dy] = (np.random.rand(dx,dy)*255).astype(int)
+
+        return image
+
+class HalfFace(RandomTransformation):
+    def __init__(self,p=0.05):
+        RandomTransformation.__init__(self,p)
+        self.__dict__.update(locals())
+        pass
+
+    def transform(self,image):
+        x_center = image.shape[0]/2.0
+        y_center = image.shape[1]/2.0
+        if np.random.rand(1) >= 0.5:
+            image[:x_center,:y_center] = (np.random.rand(image[:x_center,:y_center].shape)*255).astype(int)
+        else:
+            image[x_center:,y_center:] = (np.random.rand(image[x_center:,y_center:].shape)*255).astype(int)
+        return image
