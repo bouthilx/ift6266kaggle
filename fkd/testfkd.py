6c58558b88bc664eff5519802d28923fce419cbb
Xavier Bouthillier/ <xavier.bouthillier@gmail.com>
2013-03-28 18:29:11 -0400

    four recursiv layer

--- fkd/testfkd.py
+++ fkd/testfkd.py
@@ -1,10 +1,9 @@
 import numpy as np
 import csv
 import pylab as pl
+from scipy import ndimage
 
 from pylearn2.gui import patch_viewer
-
-from dispims import dispims
 
 NB = 36
 
@@ -48,11 +47,13 @@ print X.shape[1:3]
 
 X = X.reshape((X.shape[0],np.sqrt(X.shape[1]),np.sqrt(X.shape[1]),1))
 
-#pl.imshow(X[nonfull[i]],
-pv = patch_viewer.PatchViewer( (int(np.sqrt(NB)), int(np.sqrt(NB))), X.shape[1:3], is_color = False)
+# first layer
+Z = 0.5*0.5
+
+s = ndimage.zoom(X[0,:,:,0],Z)
+pv = patch_viewer.PatchViewer( (int(np.sqrt(NB)), int(np.sqrt(NB))), s.shape[:2], is_color = False)
 
 for i in xrange(NB):
-    print Y.shape
     for j in xrange(0,Y.shape[1],2):
         kx = Y[i,j]
         ky = Y[i,j+1]
@@ -60,6 +61,70 @@ for i in xrange(NB):
             X[i,ky-2:ky+2,kx-2:kx+2] = 0
         elif kx!=-1 or ky!=-1:
             raise RuntimeError("WTF? x=%f y=%f" % (kx, ky))
-    pv.add_patch(X[i,:,:,:], activation = 0.0, rescale = True)
+    s = ndimage.zoom(X[i,:,:,0],Z)
+    print s.shape
+    pv.add_patch(s, activation = 0.0, rescale = True)
 
-pv.show()
+pv.save("1.png")
+
+# second layer
+Z = 0.5
+sx = 96/2/2
+sy = 96/2
+
+s = ndimage.zoom(X[0,sy:sy+96/2,sx:sx+96/2,0],Z)
+pv = patch_viewer.PatchViewer( (int(np.sqrt(NB)), int(np.sqrt(NB))), s.shape[:2], is_color = False)
+
+for i in xrange(NB):
+    for j in xrange(0,Y.shape[1],2):
+        kx = Y[i,j]
+        ky = Y[i,j+1]
+        if kx > 0 and ky > 0:
+            X[i,ky-2:ky+2,kx-2:kx+2] = 0
+        elif kx!=-1 or ky!=-1:
+            raise RuntimeError("WTF? x=%f y=%f" % (kx, ky))
+    s = ndimage.zoom(X[i,sy:sy+96/2,sx:sx+96/2,0],Z)
+    print s.shape
+    pv.add_patch(s, activation = 0.0, rescale = True)
+
+pv.save("2.png")
+
+# third layer
+sx = 96/2/2
+sy = 96/2+96/4
+
+pv = patch_viewer.PatchViewer( (int(np.sqrt(NB)), int(np.sqrt(NB))), X[0,sy:sy+96/4,sx:sx+96/4,:].shape[:2], is_color = False)
+
+for i in xrange(NB):
+    for j in xrange(0,Y.shape[1],2):
+        kx = Y[i,j]
+        ky = Y[i,j+1]
+        if kx > 0 and ky > 0:
+            X[i,ky-2:ky+2,kx-2:kx+2] = 0
+        elif kx!=-1 or ky!=-1:
+            raise RuntimeError("WTF? x=%f y=%f" % (kx, ky))
+    s = X[i,sy:sy+96/4,sx:sx+96/4,:]
+    print s.shape
+    pv.add_patch(s, activation = 0.0, rescale = True)
+
+pv.save("3.png")
+
+# fourth layer
+sx = 96/2/2+96/8
+sy = 96/2+96/4
+
+pv = patch_viewer.PatchViewer( (int(np.sqrt(NB)), int(np.sqrt(NB))), X[0,sy:sy+96/8,sx:sx+96/8,:].shape[:2], is_color = False)
+
+for i in xrange(NB):
+    for j in xrange(0,Y.shape[1],2):
+        kx = Y[i,j]
+        ky = Y[i,j+1]
+        if kx > 0 and ky > 0:
+            X[i,ky-2:ky+2,kx-2:kx+2] = 0
+        elif kx!=-1 or ky!=-1:
+            raise RuntimeError("WTF? x=%f y=%f" % (kx, ky))
+    s = X[i,sy:sy+96/8,sx:sx+96/8,:]
+    print s.shape
+    pv.add_patch(s, activation = 0.0, rescale = True)
+
+pv.save("4.png")
